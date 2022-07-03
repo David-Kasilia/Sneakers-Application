296dfd4c035a569759b625163f9210c779fc1d8a
David4227 <davidkasilia001@gmail.com>
2022-07-03 17:26:00 +0300

    Create Splash Screen

---
+++ app/src/main/java/com/kasilia/creations/sneakers/SplashScreen.kt
@@ -0,0 +1,26 @@
+package com.kasilia.creations.sneakers
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler().postDelayed({
+            val splashIntent = Intent(this, MainActivity::class.java)
+            startActivity(splashIntent)
+            finish()
+
+        }, 4500)
+    }
+}
